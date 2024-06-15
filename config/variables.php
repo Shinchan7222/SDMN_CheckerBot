e12a2aa1b04190fca353b9f6c05acd9b922f4b1d
Shinchan7222 <83214906+Shinchan7222@users.noreply.github.com>
2024-06-15 18:15:33 +0530

    Update variables.php

--- config/variables.php
+++ config/variables.php
@@ -1,39 +1,23 @@
 <?php
 
-$update = json_decode(file_get_contents("php://input"));
+$update = json_decode(file_get_contents("php://input"), false);
 
-$chat_id = $update->message->chat->id;
-
-$userId = $update->message->from->id;
-
-$firstname = $update->message->from->first_name;
-
-$lastname = $update->message->from->last_name;
-
-$username = $update->message->from->username;
-
-$chattype = $update->message->chat->type;
-
-$message = $update->message->text;
-
-$message_id = $update->message->message_id;
-
-$replytomessageis = $update->message->reply_to_message->text;
-
-$data = $update->callback_query->data;
-
-$callbackfname = $update->callback_query->from->first_name;
-
-$callbacklname = $update->callback_query->from->last_name;
-
-$callbackusername = $update->callback_query->from->username;
-
-$callbackchatid = $update->callback_query->message->chat->id;
-
-$callbackuserid = $update->callback_query->message->reply_to_message->from->id;
-
-$callbackmessageid = $update->callback_query->message->message_id;
-
+$chat_id = isset($update->message->chat->id) ? $update->message->chat->id : null;
+$userId = isset($update->message->from->id) ? $update->message->from->id : null;
+$firstname = isset($update->message->from->first_name) ? $update->message->from->first_name : null;
+$lastname = isset($update->message->from->last_name) ? $update->message->from->last_name : null;
+$username = isset($update->message->from->username) ? $update->message->from->username : null;
+$chattype = isset($update->message->chat->type) ? $update->message->chat->type : null;
+$message = isset($update->message->text) ? $update->message->text : null;
+$message_id = isset($update->message->message_id) ? $update->message->message_id : null;
+$replytomessageis = isset($update->message->reply_to_message->text) ? $update->message->reply_to_message->text : null;
+$data = isset($update->callback_query->data) ? $update->callback_query->data : null;
+$callbackfname = isset($update->callback_query->from->first_name) ? $update->callback_query->from->first_name : null;
+$callbacklname = isset($update->callback_query->from->last_name) ? $update->callback_query->from->last_name : null;
+$callbackusername = isset($update->callback_query->from->username) ? $update->callback_query->from->username : null;
+$callbackchatid = isset($update->callback_query->message->chat->id) ? $update->callback_query->message->chat->id : null;
+$callbackuserid = isset($update->callback_query->message->reply_to_message->from->id) ? $update->callback_query->message->reply_to_message->from->id : null;
+$callbackmessageid = isset($update->callback_query->message->message_id) ? $update->callback_query->message->message_id : null;
 
 $live_array = array(
     'incorrect_cvc', 
